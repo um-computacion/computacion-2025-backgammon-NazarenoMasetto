0045d014926be5d222efe1c18a14a22f1aeaf8a9
Nazareno Masetto <nazamasetto@gmail.com>
2025-09-02 21:39:31 -0300

    Siguiendo con test de player

--- tests/test_player.py
+++ tests/test_player.py
@@ -17,5 +17,35 @@ class TestPlayer(unittest.TestCase):
         self.assertEqual(self.jugador.get_bar_count(), 3)
         self.jugador.remove_from_bar(2)
         self.assertEqual(self.jugador.get_bar_count(), 1)
-        self.jugador.remove_from_bar(5)  # no puede quedar en negativo
+        self.jugador.remove_from_bar(5)  
         self.assertEqual(self.jugador.get_bar_count(), 0)
+
+    def test_agregar_fuera(self):
+        self.jugador.add_off(4)
+        self.assertEqual(self.jugador.get_off_count(), 4)
+        self.jugador.add_off(1)
+        self.assertEqual(self.jugador.get_off_count(), 5)
+
+    def test_agregar_barra_valor_negativo(self):
+        self.jugador.add_to_bar(-3)  
+        self.assertEqual(self.jugador.get_bar_count(), 0)
+
+    def test_quitar_barra_valor_negativo(self):
+        self.jugador.add_to_bar(2)
+        self.jugador.remove_from_bar(-1)  
+        self.assertEqual(self.jugador.get_bar_count(), 2)
+
+    def test_agregar_fuera_valor_negativo(self):
+        self.jugador.add_off(-2)  
+        self.assertEqual(self.jugador.get_off_count(), 0)
+
+    def test_varios_movimientos(self):
+        self.jugador.add_to_bar(2)
+        self.jugador.add_off(1)
+        self.assertEqual(self.jugador.get_bar_count(), 2)
+        self.assertEqual(self.jugador.get_off_count(), 1)
+        self.jugador.remove_from_bar(1)
+        self.assertEqual(self.jugador.get_bar_count(), 1)
+
+if __name__ == "__main__":
+    unittest.main()
