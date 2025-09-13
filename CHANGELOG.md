853fa49defb4ba0405f59c28d67863b790bef409
Nazareno Masetto <nazamasetto@gmail.com>
2025-09-13 12:26:36 -0300

    Siguiendo con tests de clase game

---
+++ CHANGELOG.md
@@ -0,0 +1,5 @@
+## Changelog
+## [0.01] 13/09/2025
+
+## Agregando 
+Continuando con los test de clase game
--- tests/test_game.py
+++ tests/test_game.py
@@ -70,3 +70,39 @@ class TestBackgammonGame(unittest.TestCase):
         self.game.end_turn()
         self.assertEqual(self.game.get_available_moves(), [])
         self.assertIs(self.game.current_player(), self.pb)
+
+    def test_direction_entry_distance(self):
+        self.assertEqual(self.game.direction("white"), 1)
+        self.assertEqual(self.game.direction("black"), -1)
+        self.assertEqual(self.game.entry_point_for_die("white", 3), 2)
+        self.assertEqual(self.game.entry_point_for_die("black", 3), 23 - 2)
+        self.assertEqual(self.game.distance("white", 5, 9), 4)
+        self.assertEqual(self.game.distance("black", 9, 5), 4)
+
+    def test_can_bear_off_white_exact_y_overflow_true_false(self):
+        self.board.count_color_on_point.return_value = 0
+        self.assertTrue(self.game.can_bear_off_with("white", 23, 1))
+        self.assertTrue(self.game.can_bear_off_with("white", 22, 3))
+        def count_white(i, color):
+            return 1 if (color == "white" and i == 20) else 0
+        self.board.count_color_on_point.side_effect = count_white
+        self.assertFalse(self.game.can_bear_off_with("white", 22, 5))
+
+    def test_can_bear_off_black_exact_y_overflow_true_false(self):
+        self.board.count_color_on_point.return_value = 0
+        self.assertTrue(self.game.can_bear_off_with("black", 0, 1))
+        self.assertTrue(self.game.can_bear_off_with("black", 1, 3))
+        def count_black(i, color):
+            return 1 if (color == "black" and i == 4) else 0
+        self.board.count_color_on_point.side_effect = count_black
+        self.assertFalse(self.game.can_bear_off_with("black", 1, 5))
+
+    def test_apply_move_falla_si_hay_bar_y_src_no_es_menos1(self):
+        self.board.get_bar_count.return_value = 1
+        self.dice.roll.return_value = (3, 4)
+        self.game.roll()
+        self.assertFalse(self.game.apply_move(5, 8))
+
+    def test_apply_move_falla_sin_available(self):
+        self.board.get_bar_count.return_value = 0
+        self.assertFalse(self.game.apply_move(5, 8))
