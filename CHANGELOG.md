15c4403a3564f0620a2d1e820d76b827e102cb9d
Nazareno Masetto <nazamasetto@gmail.com>
2025-09-15 11:01:06 -0300

    COemnzando con clase Dice

---
+++ CHANGELOG.md
@@ -0,0 +1,17 @@
+## Changelog
+## [0.01] 13/09/2025
+
+## Agregando 
+Continuando con los test de clase 
+
+## Changelog
+## [0.02] 14/09/2025
+
+## Agregando 
+Terminando con los test de clase game
+
+## Changelog
+## [0.03] 15/09/2025
+
+## Agregando 
+Comnezando con clase dice
---
+++ core/dice.py
@@ -0,0 +1,25 @@
+import random
+
+class Dice:
+
+    def __init__(self):
+        self.__ultimo_resultado__ = None
+
+    def roll(self):
+
+    
+        d1 = random.randint(1, 6)
+        d2 = random.randint(1, 6)
+        self.__ultimo_resultado__ = (d1, d2)
+        return (d1, d2)
+
+    def get_last(self):
+        
+        
+        return self.__ultimo_resultado__
+
+    def is_double(self):
+        
+        if self.__ultimo_resultado__ is None:
+            return False
+        return self.__ultimo_resultado__[0] == self.__ultimo_resultado__[1]
