a644560fae5422aeef7862a1c6ef7a2709f9ef22
Nazareno Masetto <nazamasetto@gmail.com>
2025-09-26 22:02:10 -0300

    Comenzando test de clase board

--- tests/test_board.py
+++ tests/test_board.py
@@ -1,55 +1,93 @@
 import unittest
 from core.board import Board
+from core.checker import Checker
 
 class TestBoard(unittest.TestCase):
     def setUp(self):
         self.b = Board()
 
-    def test_get_point_in_range(self):
-        p0 = self.b.get_point(0)
-        self.assertIsInstance(p0, list)
-        self.assertEqual(len(p0), 0)
-        p23 = self.b.get_point(23)
-        self.assertIsInstance(p23, list)
-
-    def test_get_point_out_of_range(self):
+    def test_get_point_valid_invalid(self):
+        self.assertIsInstance(self.b.get_point(0), list)
+        self.assertIsInstance(self.b.get_point(23), list)
         self.assertIsNone(self.b.get_point(-1))
         self.assertIsNone(self.b.get_point(24))
 
-    def test_add_checker_ok(self):
-        ok = self.b.add_checker_to_point(5, "white")
-        self.assertTrue(ok)
-        p5 = self.b.get_point(5)
-        self.assertEqual(len(p5), 1)
-        self.assertEqual(p5[0].get_color(), "white")
+    def test_clear_resets_everything(self):
+        self.b.add_checker_to_point(0, "white")
+        self.b.add_to_bar("white")
+        self.b.add_to_bar("black")
+        self.b.bear_off_from("white", 0) 
+        self.b.clear()
+        for i in range(24):
+            self.assertEqual(len(self.b.get_point(i)), 0)
+        self.assertEqual(self.b.get_bar_count("white"), 0)
+        self.assertEqual(self.b.get_bar_count("black"), 0)
+        self.assertEqual(self.b.get_home_count("white"), 0)
+        self.assertEqual(self.b.get_home_count("black"), 0)
 
-    def test_add_checker_out_of_range(self):
-        ok1 = self.b.add_checker_to_point(-1, "white")
-        ok2 = self.b.add_checker_to_point(99, "black")
-        self.assertFalse(ok1)
-        self.assertFalse(ok2)
-    def test_count_color_ok_mixed(self):
+    def test_setup_start_position_distribution(self):
+        self.b.setup_start_position()
+        self.assertEqual(self.b.count_color_on_point(0, "white"), 2)
+        self.assertEqual(self.b.count_color_on_point(23, "black"), 2)
+        self.assertEqual(self.b.count_color_on_point(11, "white"), 5)
+        self.assertEqual(self.b.count_color_on_point(12, "black"), 5)
+        self.assertEqual(self.b.count_color_on_point(16, "white"), 3)
+        self.assertEqual(self.b.count_color_on_point(7,  "black"), 3)
+        self.assertEqual(self.b.count_color_on_point(18, "white"), 5)
+        self.assertEqual(self.b.count_color_on_point(5,  "black"), 5)
+
+    def test_add_and_pop_checker(self):
+        self.assertTrue(self.b.add_checker_to_point(5, "white"))
+        self.assertIsInstance(self.b.top_color_on_point(5), str)
+        self.assertEqual(self.b.top_color_on_point(5), "white")
+        popped = self.b.pop_checker_from_point(5)
+        self.assertIsInstance(popped, Checker)
+        self.assertIsNone(self.b.top_color_on_point(5))
+        self.assertIsNone(self.b.pop_checker_from_point(5))
+        self.assertFalse(self.b.add_checker_to_point(-1, "white"))
+        self.assertFalse(self.b.add_checker_to_point(99, "black"))
+        self.assertIsNone(self.b.pop_checker_from_point(-1))
+        self.assertIsNone(self.b.pop_checker_from_point(99))
+        self.assertIsNone(self.b.top_color_on_point(-1))
+        self.assertIsNone(self.b.top_color_on_point(99))
+
+    def test_count_color_on_point(self):
+        self.b.clear()
+        self.assertEqual(self.b.count_color_on_point(-1, "white"), 0)
+        self.assertEqual(self.b.count_color_on_point(99, "black"), 0)
+        self.assertEqual(self.b.count_color_on_point(3, "white"), 0)
         self.b.add_checker_to_point(3, "white")
         self.b.add_checker_to_point(3, "black")
         self.b.add_checker_to_point(3, "white")
         self.assertEqual(self.b.count_color_on_point(3, "white"), 2)
         self.assertEqual(self.b.count_color_on_point(3, "black"), 1)
 
-    def test_count_color_out_of_range_and_empty(self):
-        self.assertEqual(self.b.count_color_on_point(-5, "white"), 0)
-        self.assertEqual(self.b.count_color_on_point(100, "black"), 0)
-        self.assertEqual(self.b.count_color_on_point(10, "white"), 0)
+    def test_bar_and_home_basic(self):
+        self.b.clear()
+        self.assertEqual(self.b.get_bar_count("white"), 0)
+        self.assertEqual(self.b.get_bar_count("black"), 0)
+        self.assertEqual(self.b.get_home_count("white"), 0)
+        self.assertEqual(self.b.get_home_count("black"), 0)
+        self.b.add_to_bar("white")
+        self.b.add_to_bar("black")
+        self.assertEqual(self.b.get_bar_count("white"), 1)
+        self.assertEqual(self.b.get_bar_count("black"), 1)
+        self.assertIsInstance(self.b.remove_from_bar("white"), Checker)
+        self.assertIsNone(self.b.remove_from_bar("white"))  
+        self.assertIsInstance(self.b.remove_from_bar("black"), Checker)
+        self.assertIsNone(self.b.remove_from_bar("black"))
 
-    def test_setup_start_position(self):
-        self.b.add_checker_to_point(0, "black")
-        self.b.add_checker_to_point(23, "white")
-        self.b.add_checker_to_point(5, "white")
-        self.b.setup_start_position()
-        for i in range(24):
-            if i not in (0, 23):
-                self.assertEqual(self.b.get_point(i), [])
-        self.assertEqual(self.b.count_color_on_point(0, "white"), 2)
-        self.assertEqual(self.b.count_color_on_point(23, "black"), 2)
+    def test_can_land_cases(self):
+        self.b.clear()
+        self.assertFalse(self.b.can_land(-1, "white"))
+        self.assertFalse(self.b.can_land(24, "black"))
+        self.assertTrue(self.b.can_land(0, "white"))  
+        self.b.add_checker_to_point(1, "white")
+        self.assertTrue(self.b.can_land(1, "white"))  
+        self.b.clear()
+        self.b.add_checker_to_point(2, "black")
+        self.assertTrue(self.b.can_land(2, "white"))  
+        self.b.add_checker_to_point(2, "black")
+        self.assertFalse(self.b.can_land(2, "white"))  
 
-if __name__ == "__main__":
-    unittest.main()
+ 
