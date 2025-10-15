86a8aacd8ea05c3bf2f2edc00bb100dc02a060ce
Nazareno Masetto <nazamasetto@gmail.com>
2025-10-15 16:51:32 -0300

    Siguiendo con test de clase player

--- tests/test_player.py
+++ tests/test_player.py
@@ -36,3 +36,35 @@ class TestPlayer(unittest.TestCase):
         self.pw.add_to_bar(4)
         self.pw.remove_from_bar(-1)
         self.assertEqual(self.pw.get_bar_count(), 4)
+
+    def test_add_off_negative_is_ignored(self):
+        self.assertEqual(self.pw.get_off_count(), 0)
+        self.pw.add_off(-3)
+        self.assertEqual(self.pw.get_off_count(), 0)
+
+    def test_players_are_independent(self):
+        self.pw.add_to_bar(2)
+        self.pb.add_to_bar(5)
+        self.pw.remove_from_bar(1)
+        self.pb.remove_from_bar(2)
+        self.assertEqual(self.pw.get_bar_count(), 1)
+        self.assertEqual(self.pb.get_bar_count(), 3)
+        self.pw.add_off(-1)
+        self.pb.add_off(-1)
+        self.assertEqual(self.pw.get_off_count(), 0)
+        self.assertEqual(self.pb.get_off_count(), 0)
+
+    def test_sequence_operations_stability(self):
+        self.pw.add_to_bar(1)
+        self.pw.add_to_bar(2)
+        self.assertEqual(self.pw.get_bar_count(), 3)
+        self.pw.remove_from_bar(1)
+        self.assertEqual(self.pw.get_bar_count(), 2)
+        self.pw.remove_from_bar(5)
+        self.assertEqual(self.pw.get_bar_count(), 0)
+        self.pw.add_to_bar(0)
+        self.pw.remove_from_bar(0)
+        self.assertEqual(self.pw.get_bar_count(), 0)
+
+if __name__ == "__main__":
+    unittest.main()
