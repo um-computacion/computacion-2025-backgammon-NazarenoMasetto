45b47ca86ac88fa66136517ad502c690f2db9d5b
Nazareno Masetto <nazamasetto@gmail.com>
2025-10-23 19:05:45 -0300

    Terminando clase game

--- core/game.py
+++ core/game.py
@@ -67,7 +67,7 @@ class Game:
         if self.__turn_white__:
             return "white"
         return "black"
-    
+
     def __consume_exact__(self, dist):
         i = 0
         while i < len(self.__available__):
@@ -102,3 +102,108 @@ class Game:
             if dest >= src:
                 return -1
             return src - dest
+
+    def __distance_bar_enter__(self, color, dest):
+        if color == "white":
+            if dest < 0 or dest > 5:
+                return -1
+            return dest + 1
+        else:
+            if dest < 18 or dest > 23:
+                return -1
+            return 24 - dest
+
+    def __distance_bear_off__(self, color, src):
+        if color == "white":
+            return 24 - src
+        else:
+            return src + 1
+
+    def __there_is_behind_in_home__(self, color, src):
+        if color == "white":
+            i = 18
+            while i < src:
+                if self.__board__.count_color_on_point(i, "white") > 0:
+                    return True
+                i = i + 1
+            return False
+        else:
+            i = 5
+            while i > src:
+                if self.__board__.count_color_on_point(i, "black") > 0:
+                    return True
+                i = i - 1
+            return False
+
+    def apply_move(self, src, dest):
+        if not self.__rolled__:
+            return False
+        if len(self.__available__) == 0:
+            return False
+
+        color = self.__color__()
+
+        if self.__board__.get_bar_count(color) > 0:
+            if src != -1:
+                return False
+
+        if src == -1:
+            dist = self.__distance_bar_enter__(color, dest)
+            if dist <= 0:
+                return False
+            if not self.__consume_exact__(dist):
+                return False
+            ok = self.__board__.move_from_bar(color, dest)
+            if not ok:
+                self.__available__.append(dist)
+                return False
+            return True
+
+        if dest == -2:
+            if not self.__board__.all_in_home(color):
+                return False
+            if src < 0 or src > 23:
+                return False
+            top = self.__board__.top_color_on_point(src)
+            if top != color:
+                return False
+
+            dist = self.__distance_bear_off__(color, src)
+            if self.__consume_exact__(dist):
+                ok = self.__board__.bear_off_from(color, src)
+                if not ok:
+                    self.__available__.append(dist)
+                    return False
+                return True
+
+            if self.__there_is_behind_in_home__(color, src):
+                return False
+
+            if self.__consume_min_ge__(dist):
+                ok = self.__board__.bear_off_from(color, src)
+                if not ok:
+                    self.__available__.append(1)
+                    return False
+                return True
+
+            return False
+
+        if src < 0 or src > 23:
+            return False
+        if dest < 0 or dest > 23:
+            return False
+        if self.__board__.top_color_on_point(src) != color:
+            return False
+
+        dist = self.__distance_board__(color, src, dest)
+        if dist <= 0:
+            return False
+
+        if not self.__consume_exact__(dist):
+            return False
+
+        ok = self.__board__.move_on_board(color, src, dest)
+        if not ok:
+            self.__available__.append(dist)
+            return False
+        return True
